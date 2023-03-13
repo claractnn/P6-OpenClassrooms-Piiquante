bd579dde11ee7b53bc9aed42b4fce78a7ff47b51
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-13 17:35:14 +0100

    ajout images dans gitignore

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -20,4 +20,4 @@ const storage = multer.diskStorage({ //Utiliser fonction diskStorage pour l'enre
     }
 });
 
-module.exports = multer({storage: storage}).single('image');
+module.exports = multer({storage}).single('image');
