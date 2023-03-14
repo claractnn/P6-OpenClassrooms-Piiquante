251a0dca9ac75c2da2be9752971250ebf1f51444
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-14 16:04:17 +0100

    ajout commentaires

--- middleware/password-validator.js
+++ middleware/password-validator.js
@@ -10,14 +10,17 @@ passwordSchema
 .is().max(12) //maximum de 12 caractères
 .has().uppercase() //minimum 1 lettre en majuscule
 .has().lowercase() //contient des lettres en minuscule
-.hast().digits() //minimum 1 chiffre
+.has().digits() //minimum 1 chiffre
 .has().not().spaces() //aucun espace accepté
 .has().symbols(); //un symbole
 
+//Exporter le module 
 module.exports = (req, res, next) => {
+    //Vérifier que le mot de passe contient les bonnes propriétés écrites dans le schéma
     if (passwordSchema.validate(req.body.password)) {
         next();
     } else {
+        //Retourner un message d'erreur expliquant ce que le mot de passe doit contenir comme caractères
         return res.status(400).json({ error: `Le mot de passe doit contenir : entre 6 et 12 caractères et au minimum une lettre en majuscule, une lettre en minuscule, un chiffre et un caractère spécial`})
-    }
+    };
 };
