0460a65adfa779f1280f2cfe7a53156e241d1531
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-14 10:29:32 +0100

    ajout fichier .env

--- db-Connect.js
+++ db-Connect.js
@@ -1,8 +1,12 @@
 //Importer mongoose
 const mongoose = require('mongoose');
 
+//Importer dotenv
+const dotenv = require('dotenv');
+dotenv.config();
+
 //Connecter la base de données MongoDB et gérer l'erreur
-mongoose.connect(`mongodb+srv://piiquante:piiquante_oc@piiquante.zdq5mmz.mongodb.net/?retryWrites=true&w=majority`,
+mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@piiquante.zdq5mmz.mongodb.net/?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
