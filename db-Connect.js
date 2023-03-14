462d53ec81b1ada5ac11625d12b1ee07a0ccda72
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-14 10:33:40 +0100

    ajout variables environnement db

--- db-Connect.js
+++ db-Connect.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
+//Importer les variables d'environnement
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
 //Connecter la base de données MongoDB et gérer l'erreur
 mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@piiquante.zdq5mmz.mongodb.net/?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
