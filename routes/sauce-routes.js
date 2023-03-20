10172795e134b86fa5b4106010aaf12d3db541c5
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-20 21:54:23 +0100

    enlever multer routes delete et like

--- routes/sauce-routes.js
+++ routes/sauce-routes.js
@@ -9,7 +9,7 @@ router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-router.delete('/:id', auth, multer, sauceCtrl.deleteSauce);
-router.post('/:id/like', auth, multer, sauceCtrl.likeSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
 
 module.exports = router;
