6e2f8cb02b0c3ba85d198a0495ad256b2dbb86bf
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-27 12:35:25 +0200

    modifications commentaires

--- controllers/sauce-ctrl.js
+++ controllers/sauce-ctrl.js
@@ -1,12 +1,8 @@
-//Importer le modèle Sauce
 const Sauce = require('../models/Sauce');
-
-//Importer package filesystem de Node
 const fs = require('fs');
 
 //Afficher toutes les sauces 
 exports.getAllSauces = (req, res) => {
-    //Récupérer le tableau de toutes les sauces retournées pas la base de données
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.satuts(400).json({ error }));
@@ -21,20 +17,13 @@ exports.getOneSauce = (req, res) => {
 
 //Créer une sauce
 exports.createSauce = (req, res, next) => {
-    //Parser l'objet requête car l'objet envoyé dans la requête est sous format json mais en chaîne de caractères
     const sauceObject = JSON.parse(req.body.sauce);
-    //Supprimer le champ _id car l'id de l'objet va être généré automatiquement par la base de données
     delete sauceObject._id;
-    //Supprimer le champ userId qui correspond à la personne qui a créé l'objet, utiliser le userId qui vient du token d'authentification 
     delete sauceObject.userId;
-    //Créer le nouvel objet avec les informations transmises par l'utilisateur sans les champs précédents
     const sauce = new Sauce({
         ...sauceObject,
-        //Extraire l'userId de l'objet requête grâce au middleware
         userId: req.auth.userId,
-        //Générer l'url de l'image (car multer transmet seulemennt le nom du fichier)
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-        //Initialiser les différents paramètres de la sauce ajoutée à 0
         likes: 0,
         dislikes: 0,
         usersLiked: [],
@@ -46,21 +35,14 @@ exports.createSauce = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 };
 
-//Modifier une sauce (2 façons, selon si la requête est faite avec un fichier ou non)
+//Modifier une sauce 
 exports.modifySauce = (req, res, next) => {
-    //Extraire l'objet et regarder s'il y a un champ file
     const sauceObject = req.file ? {
-        //Parser ce que l'on récupère
         ...JSON.parse(req.body.sauce),
-        //Créer la nouvelle URL de l'image
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-        //S'il n'y a pas de fichier de transmis, récupérer l'objet directement dans le corps de la requête
     } : { ...req.body };
-    //Supprimer le userId de la requête (mesure de sécurité)
     delete sauceObject._userId;
-    //Chercher dans la base de données et récupérer la sauce 
     Sauce.findOne({ _id: req.params.id })
-        //Vérifier si l'objet correspond bien à l'utilisateur 
         .then((sauce) => {
             if (sauce.userId != req.auth.userId) {
                 res.status(401).json({ message: 'Non autorisé !' })
@@ -73,16 +55,13 @@ exports.modifySauce = (req, res, next) => {
         .catch(error => res.status(400).json({ error }))
 };
 
+//Supprimer une sauce
 exports.deleteSauce = (req, res, next) => {
-    //Récupérer l'objet en base
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => {
-            //Vérifier les droits (si l'utilisateur correspond)
             if (sauce.userId != req.auth.userId) {
                 res.status(401).json({ message: 'Non autorisé !' })
             } else {
-                //Si c'est le bon utilisateur, supprimer l'objet de la base de données et supprimer l'image du système de fichier
-                //Récupérer l'URL qui est enregistrée et recréer le chemin sur le fs à partir de celle-ci
                 const filename = sauce.imageUrl.split('/images/')[1];
                 Sauce.deleteOne({ _id: req.params.id })
                     .then(() => {
@@ -98,42 +77,33 @@ exports.deleteSauce = (req, res, next) => {
 
 //Donner son vote sur une sauce
 exports.likeSauce = (req, res) => {
-    //Récupérer l'objet en base 
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => {
-            //Définir chaque élement de l'objet sauce
             let like = sauce.likes;
             let dislike = sauce.dislikes;
             let usersLiked = sauce.usersLiked;
             let usersDisliked = sauce.usersDisliked;
             console.log(req.body);
-            //Créer une variable qui cherche dans le tableau usersDisliked ou usersLiked si une réaction existe déjà avec l'utilisateur connecté (userId)
             const voted = (usersDisliked.find(id => id === req.body.userId)) || (usersLiked.find(id => id === req.body.userId));
-            //Si la requête renvoie un like (1), ajouter 1 aux likes de l'objet sauce
             if (req.body.like === 1) {
-                if (!voted) {   //Si l'utilisateur n'est pas trouvé (voted == false)
+                if (!voted) {   
                     like += 1;
-                    usersLiked.push(req.body.userId);  //Ajouter l'userId au tableau des usersLiked
+                    usersLiked.push(req.body.userId);  
                 }
-            //Si la requête renvoie un dislike (-1), ajouter 1 aux dislikes de l'objet sauce
             } else if (req.body.like === -1) {
-                if (!voted) {   //Si l'utilisateur n'est pas trouvé (voted == false)
+                if (!voted) {   
                     dislike += 1;
-                    usersDisliked.push(req.body.userId);  //Ajouter l'userId au tableau des usersDisliked
+                    usersDisliked.push(req.body.userId);  
                 }
-            //Si la requête renvoie un 0 (si l'utilisateur retire son vote)
             } else if (req.body.like === 0) { 
-                //Vérifier si l'utilisateur a déjà voté (si son id se retrouve dans le tableau usersDisliked))
                 if (usersDisliked.find(id => id === req.body.userId)) {
-                    dislike -= 1;  //Si l'élément est trouvé, supprimer (enlever 1) aux dislikes
-                    usersDisliked = usersDisliked.filter(id => id != req.body.userId);  //Mettre à jour le tableau des dislikes des utilisateurs
-                //Vérifier si l'utilisateur a déjà voté (si son id se retrouve dans le tableau usersliked))
+                    dislike -= 1;  
+                    usersDisliked = usersDisliked.filter(id => id != req.body.userId);  
                 } else if (usersLiked.find(id => id === req.body.userId)) {
-                    like -= 1;  //Si l'élément est trouvé, supprimer (enlever 1) aux likes
-                    usersLiked = usersLiked.filter(id => id != req.body.userId);  //Mettre à jour le tableau des likes des utilisateurs
+                    like -= 1;  
+                    usersLiked = usersLiked.filter(id => id != req.body.userId); 
                 };
             };
-
             //Mettre à jour l'objet dans la base de données
             Sauce.updateOne({ _id: req.params.id },
                 {
--- controllers/user-ctrl.js
+++ controllers/user-ctrl.js
@@ -1,23 +1,12 @@
-//Importer bcrypt
 const bcrypt = require('bcrypt');
-
-//Importer jsonwebtoken
 const jwt = require('jsonwebtoken');
-
-//Importer le modèle User
 const User = require('../models/User');
-
-//Importer cryptojs pour chiffrer l'email
 const cryptojs = require('crypto-js');
-
-//Importer le plugin email-validator
 const emailValidator = require('email-validator');
 
 //Créer un compte utilisateur 
 exports.signup = (req, res, next) => {
-    //Chiffrer l'email avant de l'envoyer dans base de données (algo HmacSHA256 pour plus de sécurité)
     const emailCryptoJs = cryptojs.HmacSHA256(req.body.email, process.env.CRYPTO_MAIL).toString();
-    //Hacher le password, fonction asynchrone
     if (emailValidator.validate(req.body.email)) {
         bcrypt.hash(req.body.password, 10)
             .then(hash => {
@@ -40,18 +29,12 @@ exports.signup = (req, res, next) => {
 
 //Se connecter à son compte utilisateur
 exports.login = (req, res, next) => {
-    //chiffrer l'email avant de l'envoyer dans base de données
     const emailCryptoJs = cryptojs.HmacSHA256(req.body.email, process.env.CRYPTO_MAIL).toString();
-    //vérifier si un utilisateur existe dans la base de données et si le mot de passe transmis par le client correspond à l'utilisateur
-    //utiliser la méthode findOne de la classe User et passer un objet qui va servir de filtre (sélecteur)
     User.findOne({ email: emailCryptoJs })
-        //gérer la promesse retournée par findOne
-        //vérifier si l'utilisateur a été trouvé
         .then(user => {
             if (user === null) {
                 res.status(401).json({ message: `L'identifiant et/ou le mot de passe sont incorrects` });
             } else {
-                //comparer le mot de passe de la base de données et celui qui a été transmis
                 bcrypt.compare(req.body.password, user.password)
                     .then(valid => {
                         if (!valid) {
@@ -59,11 +42,10 @@ exports.login = (req, res, next) => {
                         } else {
                             res.status(200).json({
                                 userId: user._id,
-                                //envoyer une chaîne de caractères encodée grâce à la fonction webtoken
-                                token: jwt.sign( //ici, les données encodées(payload) à l'intérieur du token avec 3 arguments
-                                    { userId: user._id }, //créer un objet userId qui sera l'identifiant utilisateur du user pour s'assurer que la requête correspond à ce userId
-                                    process.env.SECRET_TOKEN, //créer la clé secrète pour sécuriser l'encodage
-                                    { expiresIn: process.env.TOKEN_LIFETIME } //appliquer une expiration pour le token
+                                token: jwt.sign( 
+                                    { userId: user._id },  
+                                    process.env.SECRET_TOKEN, 
+                                    { expiresIn: process.env.TOKEN_LIFETIME } 
                                 )
                             });
                         }
