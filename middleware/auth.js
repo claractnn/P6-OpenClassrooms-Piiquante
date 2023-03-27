584de4e8080041a49bdd216a913387791c6819a9
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-27 12:36:31 +0200

    modifications commentaires

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,11 @@
-//Importer le jsonwebtoken
 const jwt = require('jsonwebtoken');
 
-//Exporter le middleware
+//Décoder et vérifier l'authentification de l'utilisateur
 module.exports = (req, res, next) => {
-    //Récupérer le token, (mot clé bearer et token) et enlever la première partie
     try {
-        //Récupérer le header et diviser la chaîne de caractères en un tableau autour de l'espace qui se trouve entre le mot-clé bearer et le token 
         const token = req.headers.authorization.split(' ')[1];
-        //Décoder le token en faisant appel à la méthode verify de jwt en lui passant le token qu'on a récupéré et la clé secrète
         const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
-        //Récupérer le userId dans le token décodé et récupérer sa propriété userId
         const userId = decodedToken.userId;
-        //Rajouter cette valeur à l'objet request qui lui est transmis aux routes qui vont être appelées ensuite
-        //Crée l'objet auth dans req avec le champ userId
         req.auth = {
             userId: userId
         };
