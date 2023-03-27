0f092ecd0cd0a18045fe50e0828e705b66e6bedb
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-27 12:38:49 +0200

    modifications commentaires

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,29 +1,24 @@
-//Importer multer
 const multer = require('multer');
 
-//Créer un objet qui servira de dictionnaire
+//Générer les extensions des fichiers
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png'
 }
 
-//Crée un objet de configuration pour multer
-const storage = multer.diskStorage({ //Utiliser fonction diskStorage pour l'enregistrer sur le disque
+//Configurer le chemin et le nom des fichiers entrants vers l'API
+const storage = multer.diskStorage({ 
     destination: (req, file, callback) => {
         callback(null, 'images')
     },
     filename: (req, file, callback) => {
-        //Récupérer le nom original en séparant l'extension
         const nameFile = file.originalname.split(".")[0];
-        //Ajouter un underscore si un espace est trouvé dans le nom du fichier
         const allName = nameFile.split(' ').join('_');
         const extension = MIME_TYPES[file.mimetype];
-        //Définir le nom global du fichier avec un timestamp pour qu'il soit unique et l'extension.
         callback(null, allName + '_' + Date.now() + '.' + extension);
     }
 });
 
 module.exports = multer({ storage }).single('image');
-console.log(storage);
 
